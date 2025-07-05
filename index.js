48d47810792ffae47eda57b98efb4b80c214842c
iryna081083 <morozz.iv@gmail.com>
2025-07-05 18:29:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const crypto = require('crypto');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const signKey = '140ba6a9-1fb1-4093-8e04-84b268be597c';
+
+app.post('/generate-sign', (req, res) => {
+    const body = JSON.stringify(req.body);
+    const signature = crypto.createHmac('sha1', signKey).update(body).digest('hex');
+    res.send({ sign: signature });
+});
+
+app.listen(3000, () => {
+    console.log('Sign generator is running on port 3000');
+});
